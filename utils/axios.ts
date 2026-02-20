5c8c588fd2523c5f723aa9969eafcf4c9c779947
Matheus Vieira <rochavieira.matheus@gmail.com>
2026-02-20 01:24:46 -0300

    first commit

--- utils/axios.ts
+++ utils/axios.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-export const appApi = axios.create();
+export const appApi = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_FRONTEND_URL,
+});
 
 export const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
